7bf6cdb38975c75a832a9c90d9a2b0dce0b6dd43
saarif23 <arif23s.001@gmail.com>
2024-01-01 11:15:13 +0600

    added news collection and get all news

--- index.js
+++ index.js
@@ -21,8 +21,41 @@ const client = new MongoClient(uri, {
 const run = async () => {
 
     try {
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB Database!");
+        const db = client.db("dragon_news");
+        const categoriesCollection = db.collection("categories");
+        const newsCollection = db.collection("news");
+        // get all news
+        app.get("/all-news", async (req, res) => {
+            const allNews = await newsCollection.find({}).toArray();
+            res.send({ status: true, message: "success", data: allNews });
+        });
+
+        // get specific news
+        app.get("/news/:id", async (req, res) => {
+            const id = req.params.id;
+            const news = await newsCollection.findOne({ _id: ObjectId(id) });
+            res.send({ status: true, message: "success", data: news });
+        });
+
+        // get all categories
+        app.get("/categories", async (req, res) => {
+            const categories = await categoriesCollection.find({}).toArray();
+            res.send({ status: true, message: "success", data: categories });
+        });
+
+        // get specific categories
+        app.get("/news", async (req, res) => {
+            const name = req.query.category;
+            let newses = [];
+            if (name == "all-news") {
+                newses = await newsCollection.find({}).toArray();
+                return res.send({ status: true, message: "success", data: newses });
+            }
+            newses = await newsCollection
+                .find({ category: { $regex: name, $options: "i" } })
+                .toArray();
+            res.send({ status: true, message: "success", data: newses });
+        });
     } finally {
 
     }
